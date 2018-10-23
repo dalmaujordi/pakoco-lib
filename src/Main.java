578e7ee19ca08ba09c20237d9c8a42140b4be496
dalmaujordi <44116309+dalmaujordi@users.noreply.github.com>
2018-10-23 19:29:10 +0200

    Removed imports
    Becouse this file will not be commited in a long time, i let it clear, without imports.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
-import utils.Dual;
-import utils.LogicalOperators;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-
     }
 
 }
